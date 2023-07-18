253ac2cbe3b46c08471206dbbbc7c3b4a8d7bb14
ProfessorHusnain <mh571536@gmail.com>
2023-07-18 10:22:38 +0500

    Initial commit

---
+++ zvinodiwa/database.php
@@ -0,0 +1,26 @@
+<?php $db = new mysqli('localhost:3307','root','','agric');
+
+   /*
+// Get the filename of the current script
+$currentFile = __FILE__;
+
+// Get the directory of the current script
+$currentDir = dirname($currentFile);
+
+ 
+
+// Get the filename of the file one step back
+$parentFile = dirname($currentDir) . DIRECTORY_SEPARATOR  . 'agric.sql';
+ 
+ 
+$sql = file_get_contents($parentFile);
+
+// Execute SQL queries
+if (mysqli_multi_query($db, $sql)) {
+  echo "Database and tables created successfully.";
+} else {
+  echo "Error executing SQL queries: " . mysqli_error($db);
+}
+
+ */
+?>
